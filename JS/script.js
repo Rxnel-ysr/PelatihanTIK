c56d0a4b05bbf6b97c9212e7117f7c73365af9e4
Rxnel <myusronelyadi@gmail.com>
2025-10-28 13:07:52 +0700

    Adding theme

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,19 @@
 let btn = document.getElementById('btnSapa')
 let model = document.getElementById('model')
+let card = document.getElementById('card')
+let body = document.getElementById('body')
+let chTheme = document.getElementById('chTheme')
 
 
 btn.addEventListener('click', () => {
     model.classList.toggle('d-none')
+    card.classList.toggle('shaking')
     setTimeout(() => {
+        card.classList.toggle('shaking')
         model.classList.toggle('d-none')
-    }, 500)
+    }, 1000)
+})
+
+chTheme.addEventListener('click', () => {
+    body.classList.toggle('dark-mode')
 })
